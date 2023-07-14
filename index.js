e2afefdd4beb35fa03f45cb0602e4849d62dfe09
Aaron Madlon-Kay <aaron@madlon-kay.com>
2023-07-14 20:12:03 +0900

    Minor code tweaks

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
+
 (async () => {
-  const sig = 'SIGTERM';
   const handler = async (sig) => {
     console.log(sig, 'received; shutting down gracefully');
     for (let i = 0; i < 3; i++) {
       console.log('+');
-      await new Promise(r => setTimeout(r, 1000));
+      await sleep(1000);
     }
     process.removeListener(sig, handler);
     console.log('bye');
     process.kill(process.pid, sig);
   };
-  process.on(sig, handler);
+  process.on('SIGTERM', handler);
   console.log(process.pid);
   while (true) {
     console.log('.');
-    await new Promise(r => setTimeout(r, 1000));
+    await sleep(1000);
   }
 })()
